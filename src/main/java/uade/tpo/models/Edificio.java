b224d4af9d67b105082d1356bfca0f7032a370ad
lukascoronel <lukas.coronel00@gmail.com>
2023-09-05 21:52:02 -0300

    Update Edificio.java
    clase edificio

--- src/main/java/uade/tpo/models/Edificio.java
+++ src/main/java/uade/tpo/models/Edificio.java
@@ -6,25 +6,64 @@ import java.util.*;
  */
 public class Edificio {
 
-    /**
-     * Default constructor
-     */
-    public Edificio() {
+    private String nombre;
+    private Direccion direccion;
+    private int numeroPisos;
+    private boolean tieneAscensor;
+    private List<Unidad> unidades; 
+
+   
+    public Edificio(String nombre, Direccion direccion, int numeroPisos, boolean tieneAscensor) {
+        this.nombre = nombre;
+        this.direccion = direccion;
+        this.numeroPisos = numeroPisos;
+        this.tieneAscensor = tieneAscensor;
+        this.unidades = new ArrayList<>();
     }
 
-    /**
-     * 
-     */
-    private Direccion direccion;
+	public String getNombre() {
+		return nombre;
+	}
 
-    /**
-     * 
-     */
-    private List<Unidad> unidades;
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
 
-    /**
-     * 
-     */
-    private List<AreasComunes> areasComunes;
+	public Direccion getDireccion() {
+		return direccion;
+	}
 
+	public void setDireccion(Direccion direccion) {
+		this.direccion = direccion;
+	}
+
+	public int getNumeroPisos() {
+		return numeroPisos;
+	}
+
+	public void setNumeroPisos(int numeroPisos) {
+		this.numeroPisos = numeroPisos;
+	}
+
+	public boolean isTieneAscensor() {
+		return tieneAscensor;
+	}
+
+	public void setTieneAscensor(boolean tieneAscensor) {
+		this.tieneAscensor = tieneAscensor;
+	}
+
+	public List<Unidad> getUnidades() {
+		return unidades;
+	}
+
+	public void setUnidades(List<Unidad> unidades) {
+		this.unidades = unidades;
+	}
+
+	@Override
+	public String toString() {
+		return "Edificio [nombre=" + nombre + ", direccion=" + direccion + ", numeroPisos=" + numeroPisos
+				+ ", tieneAscensor=" + tieneAscensor + ", unidades=" + unidades + "]";
+	}
 }
