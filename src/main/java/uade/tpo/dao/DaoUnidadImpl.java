b713dcb9feef884bb2ccfb1aaec5a8b58fb41213
TKUS3R <104871413+TKUS3R@users.noreply.github.com>
2023-09-12 22:02:01 -0300

    unidad

--- src/main/java/uade/tpo/dao/DaoUnidadImpl.java
+++ src/main/java/uade/tpo/dao/DaoUnidadImpl.java
@@ -1,52 +1,63 @@
-package uade.tpo.dao;
+package dao;
 
+import java.sql.PreparedStatement;
 import java.util.List;
 
 import org.hibernate.Session;
 import org.hibernate.Transaction;
+import org.hibernate.query.Query;
 
+import uade.tpo.models.Edificio;
 import uade.tpo.models.Unidad;
 
 public class DaoUnidadImpl implements DAO<Unidad> {
 
-    private Session session;
+	private Session session;
+	
+	
+	
+	public DaoUnidadImpl(Session session) {
+		super();
+		this.session = session;
+	}
 
+	@Override
+	public List<Unidad> getAll() throws Exception {
+		Query<Unidad> getQuery = session.createQuery("FROM unidad_table", Unidad.class);
+		List<Unidad> unidades = getQuery.list();
+		return unidades;
+		
+	}
 
-    public DaoUnidadImpl(Session session) {
-        super();
-        this.session = session;
-    }
+	@Override
+	public void save(Unidad persistible) throws Exception {
+		
+			Transaction tx = session.beginTransaction();
+			session.save(persistible);
+			tx.commit();
+			
+		
+		
+	}
 
-    @Override
-    public List<Unidad> getAll() throws Exception {
-        // TODO Auto-generated method stub
-        return null;
-    }
+	@Override
+	public void update(Unidad persistible) throws Exception {
+		Transaction tx = session.beginTransaction();
+		session.save(persistible);
+		tx.commit();
+		// TODO: Comprobar con el profesor si hay que pisar el objeto
+	
+	}
 
-    @Override
-    public void save(Unidad persistible) throws Exception {
+	@Override
+	public void delete(int id) throws Exception {
+		Unidad res = session.get(Unidad.class,  id);
+		
+		if(res != null) {
+		Transaction tx = session.beginTransaction();
+		session.delete(res);
+		session.getTransaction().commit();
+		}
 
-        Transaction tx = session.beginTransaction();
-        session.save(persistible);
-        tx.commit();
-
-
-    }
-
-    @Override
-    public void update(Unidad persistible) throws Exception {
-
-    }
-
-    @Override
-    public void delete(int id) throws Exception {
-        Unidad res = session.get(Unidad.class, id);
-
-        if (res != null) {
-            Transaction tx = session.beginTransaction();
-            session.delete(res);
-            session.getTransaction().commit();
-        }
-
-    }
+	}
 }
