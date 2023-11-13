e5eefd16c95010ed6dd0a177e06fc252367871a8
LautyBona <lautybona@gmail.com>
2023-11-13 20:09:35 -0300

    testeando endpoint usuario

--- src/main/java/uade/tpo/config/SecurityConfig.java
+++ src/main/java/uade/tpo/config/SecurityConfig.java
@@ -41,7 +41,7 @@ public class SecurityConfig {
 
 	@Bean
 	public WebSecurityCustomizer webSecurityCustomizer() {
-		return (web) -> web.ignoring().requestMatchers( "auth/login");
+		return (web) -> web.ignoring().requestMatchers( "auth/login","api/usuario");
 	}
 
 	@Bean
